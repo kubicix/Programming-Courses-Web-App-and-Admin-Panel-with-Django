a4ff9bf7816958c4cf4d681a9733e980ed946de9
Kubilay Birer <kubilaybirer@hotmail.com>
2023-04-24 13:43:22 +0300

    3.9 sorunsuz

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse('anasayfa')
+data ={
+    "programlama":"programlama kategorisine ait kurslar","web-gelistirme":"web gelistirme kategorisine ait kurslar","mobil":"mobil gelistirme kategorisine ait kurslar"
+}
+def index(request):
+    courses = [
+        {'name': 'Python Programlama', 'instructor': 'Ahmet Yılmaz', 'price': 250},
+        {'name': 'Web Geliştirme', 'instructor': 'Mehmet Çelik', 'price': 200},
+        {'name': 'Makine Öğrenmesi', 'instructor': 'Ayşe Demir', 'price': 300},
+    ]
+    context = {'courses': courses}
+    return render(request, 'pages/index.html', context)
 
-def hakkimizda(request):
-    return HttpResponse('Hakkimizda : Adam olun hepinizi buluruz')
+def about(request):
+    return render(request,'pages/about.html')
 
-def iletisim(request):
-    return HttpResponse('iletisim')
+def contact(request):
+    return render(request,'pages/contact.html')
