3e2aa0ed1e1d88ab98c9ef277ea3df416be232c2
Kubilay Birer <kubilaybirer@hotmail.com>
2023-06-22 13:05:20 +0300

    updated models added category foreign key

--- courses/models.py
+++ courses/models.py
@@ -2,14 +2,21 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.name}'
 
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description=models.TextField()
     imageUrl=models.CharField(max_length=50,blank=True)
-    date=models.DateField()
+    date=models.DateField(auto_now=True)
     isActive=models.BooleanField()
     slug=models.SlugField(default="",blank=True,null=False,unique=True,db_index=True)
+    category=models.ForeignKey(Category,default=1,on_delete=models.CASCADE)
     
     def save(self,*args,**kwargs):
         self.slug = slugify(self.title)
@@ -18,9 +25,4 @@ class Course(models.Model):
     def __str__(self):
         return f'{self.title}'
 
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return f'{self.name}'
+
