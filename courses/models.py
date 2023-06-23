8927e1cd05bcc1ca282c956f12928bc4dbcb6dd3
Kubilay Birer <kubilaybirer@hotmail.com>
2023-06-23 13:25:29 +0300

    models and admin optimized

--- courses/models.py
+++ courses/models.py
@@ -4,7 +4,7 @@ from django.utils.text import slugify
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
+    slug = models.SlugField(default="",null=False,db_index=True,max_length=50)
     
     def __str__(self):
         return f'{self.name}'
@@ -16,11 +16,8 @@ class Course(models.Model):
     date=models.DateField(auto_now=True)
     isActive=models.BooleanField()
     slug=models.SlugField(default="",blank=True,null=False,unique=True,db_index=True)
-    category=models.ForeignKey(Category,default=1,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,default=1,on_delete=models.CASCADE,related_name="kurslar")
     
-    def save(self,*args,**kwargs):
-        self.slug = slugify(self.title)
-        super().save(args,kwargs)
     
     def __str__(self):
         return f'{self.title}'
