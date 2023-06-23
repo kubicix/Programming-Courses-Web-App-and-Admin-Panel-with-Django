eeb2ca87966f6434defb0eddc205f52276624417
Kubilay Birer <kubilaybirer@hotmail.com>
2023-06-23 20:06:32 +0300

    changed some migration settings

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns =[
     path('',views.index),
     path('<slug:slug>',views.details,name="course_details"),
-    path('category/<int:category_id>',views.getCoursesByCategoryId),
-    path('category/<str:category_name>',views.getCoursesByCategoryName,name='courses_by_category'),
+    path('category/<str:slug>',views.getCoursesByCategory,name='courses_by_category'),
     
 ]
