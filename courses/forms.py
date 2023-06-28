3b7169cf93ae56dec4b77581034580e7c3b74f94
Kubilay Birer <kubilaybirer@hotmail.com>
2023-06-28 12:55:33 +0300

    edited forms.py

--- courses/forms.py
+++ courses/forms.py
@@ -1,8 +1,9 @@
 from django import forms 
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(label="Kurs Başlığı",required=True,error_messages={"required":"Kurs başlığı girmelisiniz."})
     description=forms.CharField(widget=forms.Textarea)
     imageUrl=forms.CharField()
     slug=forms.SlugField()
+    #edit
     
