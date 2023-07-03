b86d5a68bad328d61bd39b7ed7a4eee9d52239cd
Kubilay Birer <kubilaybirer@hotmail.com>
2023-07-03 22:27:43 +0300

    some bugs fixed

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -24,7 +25,7 @@ urlpatterns = [
     path('',include('pages.urls')),
     path('kurslar/',include('courses.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 # courseapp
