f74e9f1d96ba8eb106ae193f9874e3af5c05b61a
Kubilay Birer <kubilaybirer@hotmail.com>
2023-07-05 17:52:32 +0300

    added login html and functions

--- account/views.py
+++ account/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth import authenticate,login
 # Create your views here.
 
 def user_login(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        
+        user=authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect("index")
+        else:
+            return render(request,"account/login.html",{"error":"Kullanıcı adı ya da parola hatalı."})
+    else:
+        return render(request,"account/login.html")
+    
     return render(request,"account/login.html")
 
 def user_register(request):
