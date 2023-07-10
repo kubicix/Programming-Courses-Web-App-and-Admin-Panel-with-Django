0c44134c07bce996b0656dbe333aa302762e38bf
Kubilay Birer <kubilaybirer@hotmail.com>
2023-07-10 23:26:27 +0300

    authentication form inherited and upgraded

--- account/views.py
+++ account/views.py
@@ -3,13 +3,15 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
+
+from account.forms import LoginUserForm
 # Create your views here.
 
 def user_login(request):
     if request.user.is_authenticated and "next" in request.GET:
         return render(request,"account/login.html",{"error":"Yetkiniz yok"})
     if request.method=="POST":
-        form = AuthenticationForm(request,data=request.POST)
+        form = LoginUserForm(request,data=request.POST)
         if form.is_valid():  
             username=form.cleaned_data.get("username")
             password=form.cleaned_data.get("password")
@@ -32,7 +34,7 @@ def user_login(request):
             return render(request,"account/login.html",{"form":form})
             #messages.form
     else:
-        form = AuthenticationForm()
+        form = LoginUserForm()
         return render(request,"account/login.html",{"form":form})
     
     return render(request,"account/login.html")
