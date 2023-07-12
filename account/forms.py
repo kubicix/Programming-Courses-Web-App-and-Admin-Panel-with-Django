0fac254c940b1f361d62eab125059a802c3e439b
Kubilay Birer <kubilaybirer@hotmail.com>
2023-07-12 22:36:50 +0300

    clean email and password inherited

--- account/forms.py
+++ account/forms.py
@@ -22,9 +22,26 @@ class LoginUserForm(AuthenticationForm):
 class NewUserForm(UserCreationForm):
     class Meta:
         model=User
-        fields = ("username","email",)
+        fields = ("first_name","last_name","username","email",)
     
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.fields()
+        self.fields["first_name"].widget=widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["last_name"].widget=widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["password1"].widget=widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["password2"].widget=widgets.PasswordInput(attrs={"class":"form-control"})
+        self.fields["username"].widget=widgets.TextInput(attrs={"class":"form-control"})
+        self.fields["email"].widget=widgets.EmailInput(attrs={"class":"form-control"})
+        self.fields["email"].required=True
+    
+  
+        
+    def clean_email(self):
+        email=self.cleaned_data.get("email")
+        
+        if User.objects.filter(email=email).exists():
+            self.add_error("email","Mail already exists in the database.")
+        
+        return email
+        
             
