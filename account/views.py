1d0b681d73e9443760d42ef659b62907ac12c0d8
Kubilay Birer <kubilaybirer@hotmail.com>
2023-07-13 21:17:02 +0300

    password change form added

--- account/views.py
+++ account/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
 from account.forms import LoginUserForm, NewUserForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 # Create your views here.
 
 def user_login(request):
@@ -57,7 +57,19 @@ def user_register(request):
     else:
         form = NewUserForm()
         return render(request,"account/register.html",{"form":form})
-        
+
+def change_password(request):
+    if request.method=="POST":
+        form = PasswordChangeForm(request.user,request.POST)
+        if form.is_valid():
+            user=form.save()
+            update_session_auth_hash(user)
+            messages.success(request,"Password Changed")
+            return redirect("change_password")
+        else:
+            return render(request,"account/change-password.html",{"form":form})
+    form = PasswordChangeForm(request.user)
+    return render(request,"account/change-password.html",{"form":form})
 
 def user_logout(request):
     messages.add_message(request,messages.SUCCESS,"Çıkış Başarılı.")
